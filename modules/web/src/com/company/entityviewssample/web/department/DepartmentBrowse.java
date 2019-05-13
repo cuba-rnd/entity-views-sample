9f1f5bc3e0582bfe4e786049a6b3b688566dbf71
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-05-13 10:07:04 +0300

    Using instance name instead of method call, removed declarative view from the XML

--- modules/web/src/com/company/entityviewssample/web/department/DepartmentBrowse.java
+++ modules/web/src/com/company/entityviewssample/web/department/DepartmentBrowse.java
@@ -3,21 +3,25 @@ package com.company.entityviewssample.web.department;
 import com.company.entityviewssample.entity.MutableDepartment;
 import com.company.entityviewssample.entity.ReadonlyDepartmentAndManager;
 import com.company.entityviewssample.service.DepartmentsService;
+import com.haulmont.addons.cuba.entity.views.scan.ViewsConfiguration;
 import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.MetadataTools;
 import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.ScreenBuilders;
-import com.haulmont.cuba.gui.actions.list.RemoveAction;
 import com.haulmont.cuba.gui.components.Action;
-import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.DialogAction;
 import com.haulmont.cuba.gui.components.GroupTable;
-import com.haulmont.cuba.gui.components.Table;
-import com.haulmont.cuba.gui.model.DataContext;
-import com.haulmont.cuba.gui.screen.*;
-import com.company.entityviewssample.entity.Department;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.screen.Install;
+import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
+import com.haulmont.cuba.gui.screen.LookupComponent;
+import com.haulmont.cuba.gui.screen.StandardLookup;
+import com.haulmont.cuba.gui.screen.Subscribe;
+import com.haulmont.cuba.gui.screen.Target;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import java.util.List;
 
 @UiController("entityviewssample_Department.browse")
@@ -30,17 +34,31 @@ public class DepartmentBrowse extends StandardLookup<ReadonlyDepartmentAndManage
     private DepartmentsService departmentsService;
 
     @Inject
-    private ScreenBuilders screenBuilders;
+    private ViewsConfiguration viewsConfiguration;
 
     @Inject
     private GroupTable<ReadonlyDepartmentAndManager> departmentsTable;
 
     @Inject
+    private CollectionContainer<ReadonlyDepartmentAndManager> departmentsDc;
+
+    @Inject
+    private ScreenBuilders screenBuilders;
+
+    @Inject
     private Dialogs dialogs;
+
+    @Inject
+    private MetadataTools metadataTools;
 
     @Install(to = "departmentsDl", target = Target.DATA_LOADER)
     private List<ReadonlyDepartmentAndManager> departmentsDlLoadDelegate(LoadContext<ReadonlyDepartmentAndManager> loadContext) {
         return departmentsService.getReadonlyDepartmentData(loadContext);
+    }
+
+    @Subscribe
+    private void onInit(InitEvent event) {
+        departmentsDc.setView(viewsConfiguration.getViewByInterface(ReadonlyDepartmentAndManager.class));
     }
 
     @Subscribe("departmentsTable.create")
@@ -66,7 +84,7 @@ public class DepartmentBrowse extends StandardLookup<ReadonlyDepartmentAndManage
         if (selected != null) {
             dialogs.createOptionDialog()
                     .withCaption("Confirm")
-                    .withMessage("Do you want to delete department \""+selected.getName()+"\"?")
+                    .withMessage("Do you want to delete department \""+ metadataTools.getInstanceName(selected)+"\"?")
                     .withActions(
                             new DialogAction(DialogAction.Type.YES, Action.Status.PRIMARY).withHandler(e -> {
                                 departmentsService.removeDepartment(selected);
